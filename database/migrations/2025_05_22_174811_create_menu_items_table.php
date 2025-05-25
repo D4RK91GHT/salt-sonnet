c52402bfec8026e20a4364e4d96a87f1a1305500
D4RK91GHT <rahulmajumdar400@gmail.com>
2025-05-25 23:26:18 +0530

    MENU ITEMS MIGRATION UPDATED

--- database/migrations/2025_05_22_174811_create_menu_items_table.php
+++ database/migrations/2025_05_22_174811_create_menu_items_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('menu_items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('includes', 255)->nullable();
+            $table->string('includes', 355)->nullable();
             $table->text('description')->nullable();
             $table->foreignId('category_id')->constrained('menu_categories');
             $table->decimal('mrp', 10, 2)->default(0);
             $table->decimal('discount', 10, 2)->default(0);
-            $table->decimal('rate', 10, 2)->virtualAs('mrp - discount');
+            $table->decimal('rate', 10, 2)->virtualAs('mrp - (mrp * (discount / 100))');
             $table->decimal('gst', 5, 2)->default(0);
-            $table->decimal('price', 8, 2)->default(0);
+            $table->decimal('price', 8, 2)->virtualAs('LEAST(rate + (rate * (gst / 100)), mrp)');
             $table->boolean('is_available')->default(true);
             $table->string('image')->nullable();
             $table->timestamps();
