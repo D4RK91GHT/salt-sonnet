416916011d15e0f08155c254f9c21835da7f0376
D4RK91GHT <rahulmajumdar400@gmail.com>
2025-05-31 23:37:51 +0530

    web pages are moved into directory

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/admin-app.css', 'resources/js/admin-app.js'],
+            input: ['resources/css/admin-app.css', 'resources/js/admin-app.js', 'resources/css/web.css', 'resources/js/web.js'],
             refresh: true,
         }),
     ],
