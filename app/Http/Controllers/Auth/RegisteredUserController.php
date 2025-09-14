c09f6c92fa0377a21c03c059f8714f2108a4fa6a
Dipak Majumdar <rahulmajumdar400@gmail.com>
2025-09-14 19:10:16 +0530

    Event, Listner and Queue created and implemented to send email to newly registered customer

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Auth\Events\Registered;
+use App\Events\UserRegistered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -41,7 +41,7 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        event(new Registered($user));
+        event(new UserRegistered($user));
 
         Auth::login($user);
 
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Bistro</title>
+</head>
+<body>
+    <h1>Welcome, {{ $user->name }}!</h1>
+    <p>Thank you for registering with our application. We're excited to have you on board!</p>
+    
+    <p>If you have any questions, feel free to contact our support team.</p>
+    
+    <p>Best regards,<br>
+    The Team</p>
+</body>
+</html>
