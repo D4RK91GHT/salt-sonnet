b8fe07122f9325b2723f18c3b5141a2b6c2cf5dd
Dipak Majumdar <rahulmajumdar400@gmail.com>
2025-09-21 00:52:16 +0530

    cors setting applied

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,22 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->encryptCookies(except: [
             'guest_identifier',
         ]);
+        
+        // CORS configuration
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+            'sanctum/csrf-cookie',
+            'login',
+            'logout',
+            'register'
+        ]);
+        
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: ['*']);
+        
+        // Enable CORS for all routes
+        $middleware->web(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->api(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
